54b31a918333a681c1ac9c664af1af6c5f7571e0
RusSev <33976370+RusSev@users.noreply.github.com>
2020-03-05 11:34:00 +0300

    Revert "Test"

--- igornotrobot.py
+++ igornotrobot.py
@@ -1,33 +1,8 @@
 import telebot
 import pyowm
-import flask
-from telebot import types
-from config import *
-from igornotrobot import bot
-import os
- 
-server = flask.Flask(__name__)
- 
- 
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    bot.process_new_updates([types.Update.de_json(
-         flask.request.stream.read().decode("utf-8"))])
-    return "!", 200
- 
- 
-@server.route('/', methods=["GET"])
-def index():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
-    return "Hello from Heroku!", 200
- 
- 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 owm = pyowm.OWM('63294dff63640c2518e4cfccb3fabcde', language="ru")
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot("1113301191:AAGiCwjps9e7waWM9ttxWBN2l1VuOQRDAoQ")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
